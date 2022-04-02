52893f5f9a0f1f19c0348b42add0a0731cbc745e
kshitij-halankar <halanka@uwindsor.ca>
2022-04-02 19:06:12 -0400

    fixing merge issues

--- WebSearchEngine/src/websearchengine/WebSearchEngine.java
+++ WebSearchEngine/src/websearchengine/WebSearchEngine.java
@@ -1,11 +1,14 @@
 package websearchengine;
 
+import static websearchengine.Functions.quickSort;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class WebSearchEngine {
 	static Scanner obj = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		InvertedIndex invertedIndex = new InvertedIndex();
 
@@ -21,12 +24,24 @@ public class WebSearchEngine {
 
 		// Printing the words present in the dictionary
 		invertedIndex.createDictionary();
-		System.out.println("Number of words: " + invertedIndex.getDictionary().size());
 		ArrayList<String> dictionary = invertedIndex.getDictionary();
-		
-		  for (int i = 0; i < dictionary.size(); i++) {
-		  System.out.println(dictionary.get(i)); }
-		 
+		for (int i = 0; i < dictionary.size(); i++) {
+			if (invertedIndex.trie.search(invertedIndex.trie.dictionary.get(i), invertedIndex.trie.root)) {
+
+				IndexObject indexObject = invertedIndex.trie.find(invertedIndex.trie.dictionary.get(i),
+						invertedIndex.trie.root).wordObject;
+				quickSort(indexObject, 0, indexObject.indicesHolder.size() - 1);
+				invertedIndex.trie.setIndexObject(invertedIndex.trie.root, indexObject);
+
+				for (int j = 0; j < indexObject.indicesHolder.size(); j++) {
+					System.out.println(
+							invertedIndex.trie.dictionary.get(i) + " : Key  : " + indexObject.indicesHolder.get(j).url
+									+ "........" + indexObject.indicesHolder.get(j).frequency);
+				}
+
+			}
+
+		}
 
 		// Storing the trie object in output file
 		invertedIndex.createSerializableFile();
@@ -35,12 +50,13 @@ public class WebSearchEngine {
 
 		// Printing the links in which the word tutorialspoint is present along with the
 		// frequency
+		// frequency
 		if (inputTrie.search("geeksforgeeks", inputTrie.root) == true) {
-			HashMap<String, Integer> tempMap = inputTrie.find("geeksforgeeks", inputTrie.root).wordObject
+			ArrayList<LinkIndex> tempMap = inputTrie.find("geeksforgeeks", inputTrie.root).wordObject
 					.getindicesHolder();
-			for (String strKey : tempMap.keySet()) {
-				System.out.print("tutorialspoint" + " : Key  : " + strKey + "........");
-				System.out.print(tempMap.get(strKey) + "\n");
+			for (LinkIndex temp : tempMap) {
+				System.out.print("geeksforgeeks" + " : Key  : " + temp.url + "........");
+				System.out.print(temp.frequency + "\n");
 			}
 		} else {
 			System.out.println("Not Found");
@@ -53,7 +69,7 @@ public class WebSearchEngine {
 
 //		if not found dictionary - edit distance
 
-		//Editdistance
+		// Editdistance
 		Editdistance editdistance = new Editdistance();
 		System.out.println("Enter a word that needs to be searched");
 		String uword = obj.next();
